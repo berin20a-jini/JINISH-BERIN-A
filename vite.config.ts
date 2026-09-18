21a86122fe514cd9a1fc31891c93ccb8597c329c
berin20a-jini <berin20a@gmail.com>
2026-09-18 09:32:29 +0530

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/JINISH-BERIN-A/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
